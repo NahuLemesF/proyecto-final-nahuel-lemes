9a53d580c2d902f285b97740b187209027cfd119
NahuLemesF <nahulem@gmail.com>
2023-11-15 13:07:37 -0300

    Se consume API para crear Usuario

--- src/views/components/SignInForm/SignInForm.jsx
+++ src/views/components/SignInForm/SignInForm.jsx
@@ -1,14 +1,35 @@
 import { Input } from '../Input/Input'
+import './SignInForm.css'
+import { createUser } from '../../../services/user'
 
 export const SignInForm = () => {
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+    let body = {}
+    for (const element of event.target.elements) {
+      if (element.name) {
+        body = { ...body, [element.name]: element.value }
+      }
+    }
+    create(body, event)
+  }
+
+  const create = (body, event) => {
+    createUser(body)
+      .then(user => {
+        window.alert('Se creó correctamente el usuario ' + user.firstName)
+        event.target.reset()
+      })
+  }
+
   return (
-    <form action=''>
+    <form onSubmit={handleSubmitForm} className='card' action=''>
       <fieldset>
-        <Input id='nombre' name='nombre' textLabel='Nombre:' type='text' />
-        <Input id='apellido' name='apellido' textLabel='Apellido' type='text' />
-        <Input type='text' name='email' id='email' textLabel='E-mail:' />
-        <Input type='text' name='password' id='password' textLabel='Contraseña:' />
-        <button>Regístrarme</button>
+        <Input type='text' id='nombre' name='firstName' textLabel='Nombre:' required />
+        <Input type='text' id='apellido' name='lastName' textLabel='Apellido' required />
+        <Input type='text' id='email' name='email' textLabel='E-mail:' required />
+        <Input type='password' id='password' name='password' textLabel='Contraseña:' required />
+        <button type='submit'>Regístrarme</button>
       </fieldset>
     </form>
 
