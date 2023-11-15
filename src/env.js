945cf3ebeae7f1a4db187b724aa94b16b18eb54f
NahuLemesF <nahulem@gmail.com>
2023-11-15 13:07:08 -0300

    Se crearon env.js y user.js

---
+++ src/env.js
@@ -0,0 +1,3 @@
+export const API_USER = 'https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/user'
+
+export const API_TODO = 'https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/todo'
---
+++ src/services/user.js
@@ -0,0 +1,25 @@
+import { API_USER } from '../env'
+
+export const createUser = (body) => {
+  return fetch(API_USER, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => response.json())
+    .then(response => response.user)
+}
+
+export const authenticateUser = (body) => {
+  return fetch(API_USER + '/auth', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(response => response.json())
+    .then(response => response.user)
+}
