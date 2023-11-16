c823adc0471e25ca99b75734f201202864d84415
NahuLemesF <nahulem@gmail.com>
2023-11-16 11:43:51 -0300

    Arreglo de Imagenes para Deploy

--- src/views/components/Header/Header.jsx
+++ src/views/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom'
 import './Header.css'
+import Logo from '/src/assets/img/logo.svg'
+import Banner from '/src/assets/img/brand.png'
 
 export const Header = () => {
   return (
     <header className='header'>
       <div className='header__banner'>
-        <img className='header__banner--logo' src='src/assets/img/logo.svg' alt='Capi Logo' />
-        <img src='src/assets/img/brand.png' alt='banner' className='header__banner--brand' />
+        <img className='header__banner--logo' src={Logo} alt='Capi Logo' />
+        <img src={Banner} alt='banner' className='header__banner--brand' />
       </div>
 
       <nav className='header__nav'>
