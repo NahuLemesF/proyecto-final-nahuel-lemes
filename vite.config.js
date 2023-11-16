a8d7a03e2915501349a6c4de3bbda2587e5e5f6e
NahuLemesF <nahulem@gmail.com>
2023-11-16 10:34:33 -0300

    Se agrega deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: https://NahuLemesF.github.io/proyecto-final-nahuel-lemes/
+  base: 'https://NahuLemesF.github.io/proyecto-final-nahuel-lemes/'
 })
